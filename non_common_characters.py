76635f4718c13eb8aac551ffafaa86a9d1886081
Jyothulachakraveni <102216487+Jyothulachakraveni@users.noreply.github.com>
2022-11-01 18:39:00 +0530

    n=input() m=input() s=[] n=n.lower() m=m.lower() for i in range(len(n)):     if n[i] not in m and n[i]!=" ":         if n[i] not in s:             s.append(n[i]) for i in range(len(m)):     if m[i] not in n and m[i]!=" ":         if m[i] not in s:             s.append(m[i]) s.sort() for i in s:     print(i,end="")

---
+++ non_common_characters.py
@@ -0,0 +1,16 @@
+n=input()
+m=input()
+s=[]
+n=n.lower()
+m=m.lower()
+for i in range(len(n)):
+    if n[i] not in m and n[i]!=" ":
+        if n[i] not in s:
+            s.append(n[i])
+for i in range(len(m)):
+    if m[i] not in n and m[i]!=" ":
+        if m[i] not in s:
+            s.append(m[i])
+s.sort()
+for i in s:
+    print(i,end="")
