ec7b8eb4d5055ecbe839ed4b1727e35d52f78854
Jyothulachakraveni <102216487+Jyothulachakraveni@users.noreply.github.com>
2022-11-01 18:54:19 +0530

    n=list(map(str,input().split())) m=list(map(str,input().split())) s=[] for i in range(len(m)):     if str(m[i]).lower() in n and m[i]!=" " or m[i] in n and m[i]!=' ':         if m[i] not in s:             s.append(str(m[i]).lower()) for i in range(len(n)):     if str(n[i]).lower() in m and n[i]!=" ":         if n[i] not in s:             s.append(str(n[i]).lower()) print(len(s))

---
+++ common_words_-1.py
@@ -0,0 +1,12 @@
+n=list(map(str,input().split()))
+m=list(map(str,input().split()))
+s=[]
+for i in range(len(m)):
+    if str(m[i]).lower() in n and m[i]!=" " or m[i] in n and m[i]!=' ':
+        if m[i] not in s:
+            s.append(str(m[i]).lower())
+for i in range(len(n)):
+    if str(n[i]).lower() in m and n[i]!=" ":
+        if n[i] not in s:
+            s.append(str(n[i]).lower())
+print(len(s))
