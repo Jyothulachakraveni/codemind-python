64d2a533feacb63d0bee7ed8d8b7f973a7ab0c4a
Jyothulachakraveni <102216487+Jyothulachakraveni@users.noreply.github.com>
2022-11-06 19:23:32 +0530

    def fun(n):     n=str(n)     k=n[::-1]     if k==n:         return k     else:         n=int(n)         return fun(n+1) def gun(n):     n=str(n)     k=n[::-1]     if k==n:         return k     else:         n=int(n)         return gun(n-1) n=int(input()) g=fun(n+1) h=gun(n-1) g=int(g) h=int(h) if abs(n-g)>abs(n-h):     print(h) elif abs(n-g)==abs(n-h):     print(h,g) else:     print(g)

---
+++ Closest_Palindrome_.py
@@ -0,0 +1,27 @@
+def fun(n):
+    n=str(n)
+    k=n[::-1]
+    if k==n:
+        return k
+    else:
+        n=int(n)
+        return fun(n+1)
+def gun(n):
+    n=str(n)
+    k=n[::-1]
+    if k==n:
+        return k
+    else:
+        n=int(n)
+        return gun(n-1)
+n=int(input())
+g=fun(n+1)
+h=gun(n-1)
+g=int(g)
+h=int(h)
+if abs(n-g)>abs(n-h):
+    print(h)
+elif abs(n-g)==abs(n-h):
+    print(h,g)
+else:
+    print(g)
